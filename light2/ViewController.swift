0ea4a2c5a5a7f54c3a9591e7c6fe46ef7f922795
Sergey Medvedev <devgod@mail.ru>
2022-11-07 12:18:30 +0300

    xx-2.3

--- light2/ViewController.swift
+++ light2/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: - ViewDidLoad
         redSquare.alpha = 1
         redSquare.layer.cornerRadius = redSquare.frame.width / 2
         
