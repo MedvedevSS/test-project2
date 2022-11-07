037143f650ece1a471c9dfc36df71700ac7a8496
Sergey Medvedev <devgod@mail.ru>
2022-11-07 12:36:06 +0300

    22

---
+++ light2/ViewController_BACKUP_9452.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  light2
+//
+//  Created by Sergey Medvedev on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var yellowSquare: UIView!
+    @IBOutlet weak var greenSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+<<<<<<< HEAD
+        // MARK: - View Did Load
+=======
+        // MARK: - ViewDidLoad
+>>>>>>> feature/xx-2_make_some_change
+        redSquare.alpha = 1
+        redSquare.layer.cornerRadius = redSquare.frame.width / 2
+        
+        yellowSquare.alpha = 0.3
+        yellowSquare.layer.cornerRadius = redSquare.frame.width / 2
+        
+        greenSquare.alpha = 0.3
+        greenSquare.layer.cornerRadius = redSquare.frame.width / 2
+        
+        // Do any additional setup after loading the view.
+    }
+    // MARK: - Actions
+    @IBAction func startButton(_ sender: UIButton) {
+        if redSquare.alpha == 1 {
+            redSquare.alpha = 0.3
+            yellowSquare.alpha = 0.3
+            greenSquare.alpha = 1
+        } else if greenSquare.alpha == 1{
+            greenSquare.alpha = 0.3
+            redSquare.alpha = 0.3
+            yellowSquare.alpha = 1
+        } else if yellowSquare.alpha == 1 {
+            yellowSquare.alpha = 0.3
+            greenSquare.alpha = 0.3
+            redSquare.alpha = 1
+        }
+    }
+    
+
+}
+
